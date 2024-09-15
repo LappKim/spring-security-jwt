98a65f95daa2a290a34d0a4d248a07726ad2fdd4
snake <snake@host.docker.internal>
2024-09-15 16:05:22 +0900

    김현석
    1. MyController 추가

---
+++ src/main/java/com/slowstarter/controller/MyController.java
@@ -0,0 +1,37 @@
+package com.slowstarter.controller;
+
+import java.util.Collection;
+import java.util.Iterator;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+public class MyController {
+    @GetMapping(value = "/my")
+    public String myPage() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        String       username = authentication.getName();
+        StringBuffer sbRoles  = new StringBuffer();
+
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        Iterator<? extends GrantedAuthority> iterator = authorities.iterator();
+
+        while(iterator.hasNext() == true) {
+            GrantedAuthority auth = iterator.next();
+            if(sbRoles.length() != 0) {
+                sbRoles.append(", ");
+            }
+            sbRoles.append(auth.getAuthority());
+        }
+
+        return String.format("my Page username = %s, roles = %s", username, sbRoles.toString());
+    }
+}
